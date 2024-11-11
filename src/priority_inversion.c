208de11bff63054f94f40fa98e7c50ecbabc8d0f
Noah Lomu <noah.lomu@gmail.com>
2024-11-11 13:09:16 -0700

    Move priority inversion to test.c and implement TEST_ASSERT statements

--- src/priority_inversion.c
+++ src/priority_inversion.c
@@ -1,91 +1,66 @@
-/**
- * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+// /**
+//  * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+//  *
+//  * SPDX-License-Identifier: BSD-3-Clause
+//  */
 
-#include <stdio.h>
+// #include <stdio.h>
 
-#include "FreeRTOS.h"
-#include "task.h"
-#include "semphr.h"
-#include "pico/stdlib.h"
-#include "pico/multicore.h"
-#include "pico/cyw43_arch.h"
+// #include "FreeRTOS.h"
+// #include "task.h"
+// #include "semphr.h"
+// #include "pico/stdlib.h"
+// #include "pico/multicore.h"
+// #include "pico/cyw43_arch.h"
 
-#define LOW_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
-#define MEDIUM_TASK_PRIORITY   ( tskIDLE_PRIORITY + 2UL )
-#define HIGH_TASK_PRIORITY     ( tskIDLE_PRIORITY + 3UL )
-#define LOW_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define MEDIUM_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define HIGH_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+// #define LOW_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+// #define MEDIUM_TASK_PRIORITY   ( tskIDLE_PRIORITY + 2UL )
+// #define HIGH_TASK_PRIORITY     ( tskIDLE_PRIORITY + 3UL )
+// #define LOW_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+// #define MEDIUM_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+// #define HIGH_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-SemaphoreHandle_t semaphore;
+// SemaphoreHandle_t semaphore;
 
-void high_priority_task(__unused void *params) {
-    printf("Hello from high priority thread.\n");
-    vTaskDelay(200);
-    while(1){
-        printf("Trying to take semaphore in high priority thread.\n");
-        bool semaphore_pass = xSemaphoreTake(semaphore, portMAX_DELAY);
-        if(semaphore_pass){
-            printf("We have the semaphore in high priority!\n");
-            for(int i = 0; i < 100000000; i++) {
-                if (i % 10000000 == 0);
-            }
-            xSemaphoreGive(semaphore);
-        }
-    }
-}
+// void high_priority_task(__unused void *params) {
+//     printf("Hello from high priority thread.\n");
+//     vTaskDelay(200);
+//     while(1){
+//         printf("Trying to take semaphore in high priority thread.\n");
+//         bool semaphore_pass = xSemaphoreTake(semaphore, portMAX_DELAY);
+//         if(semaphore_pass){
+//             printf("We have the semaphore in high priority!\n");
+//             for(int i = 0; i < 100000000; i++) {
+//                 if (i % 10000000 == 0);
+//             }
+//             xSemaphoreGive(semaphore);
+//         }
+//     }
+// }
 
-void medium_priority_task(__unused void *params) {
-    printf("Hello from medium priority thread.\n");
-    vTaskDelay(100);
-    while(1){
-        for(int i = 0; i < 1000000000; i++) {
-            if (i % 100000000 == 0) {
-                printf("Medium priority thread: counted to %d.\n", i);
-            }
-        }
-    }
-}
+// void medium_priority_task(__unused void *params) {
+//     printf("Hello from medium priority thread.\n");
+//     vTaskDelay(100);
+//     while(1){
+//         for(int i = 0; i < 1000000000; i++) {
+//             if (i % 100000000 == 0) {
+//                 printf("Medium priority thread: counted to %d.\n", i);
+//             }
+//         }
+//     }
+// }
 
-void low_priority_task(__unused void *params) {
-    printf("Hello from low priority thread.\n");
-    while(1){
-        printf("Trying to take semaphore in low priority thread.\n");
-        bool semaphore_pass = xSemaphoreTake(semaphore, 500);
-        if(semaphore_pass){
-            printf("We have the semaphore in low priority!\n");
-            for(int i = 0; i < 100000000; i++) {
-                if (i % 10000000 == 0);
-            }
-            xSemaphoreGive(semaphore);
-        }
-    }
-}
-
-int main( void )
-{
-    stdio_init_all();
-    hard_assert(cyw43_arch_init() == PICO_OK);
-    sleep_ms(10000); // Wait to connect to Pico
-    printf("Main: starting code:\n");
-
-    TaskHandle_t hp_task, mp_task, lp_task;
-    // semaphore = xSemaphoreCreateBinary();
-    // Using a mutex instead of a binary solves priority inversion 
-    //  because it implements priority inheritence.
-    semaphore = xSemaphoreCreateMutex(); 
-    xSemaphoreGive(semaphore);
-    xTaskCreate(low_priority_task, "Low Priority Thread",
-                LOW_TASK_STACK_SIZE, NULL, LOW_TASK_PRIORITY, &lp_task);
-    xTaskCreate(medium_priority_task, "Medium Priority Thread",
-                MEDIUM_TASK_STACK_SIZE, NULL, MEDIUM_TASK_PRIORITY, &mp_task);
-    xTaskCreate(high_priority_task, "High Priority Thread",
-                HIGH_TASK_STACK_SIZE, NULL, HIGH_TASK_PRIORITY, &hp_task);
-    
-    printf("About to start task scheduler.\n");
-    vTaskStartScheduler();
-    return 0;
-}
+// void low_priority_task(__unused void *params) {
+//     printf("Hello from low priority thread.\n");
+//     while(1){
+//         printf("Trying to take semaphore in low priority thread.\n");
+//         bool semaphore_pass = xSemaphoreTake(semaphore, 500);
+//         if(semaphore_pass){
+//             printf("We have the semaphore in low priority!\n");
+//             for(int i = 0; i < 100000000; i++) {
+//                 if (i % 10000000 == 0);
+//             }
+//             xSemaphoreGive(semaphore);
+//         }
+//     }
+// }
--- test/test.c
+++ test/test.c
@@ -11,14 +11,123 @@
 #include <pico/cyw43_arch.h>
 #include "busy_threads.h"
 
-#define TEST_RUNNER_PRIORITY    (tskIDLE_PRIORITY + 5UL)
-#define TEST_THREAD_MEDIUM_PRIORITY  (TEST_RUNNER_PRIORITY - 1UL)
-#define TEST_THREAD_LOW_PRIORITY  (TEST_RUNNER_PRIORITY - 2UL)
-#define TEST_THREAD_STACK_SIZE  (configMINIMAL_STACK_SIZE)
+#define TEST_RUNNER_PRIORITY        (tskIDLE_PRIORITY + 5UL)
+#define TEST_THREAD_HIGH_PRIORITY   (TEST_RUNNER_PRIORITY - 1UL)
+#define TEST_THREAD_MEDIUM_PRIORITY (TEST_RUNNER_PRIORITY - 2UL)
+#define TEST_THREAD_LOW_PRIORITY    (TEST_RUNNER_PRIORITY - 3UL)
+#define TEST_THREAD_STACK_SIZE      (configMINIMAL_STACK_SIZE)
 
 void setUp(void) {}
 
 void tearDown(void) {}
+
+SemaphoreHandle_t semaphore;
+void high_priority_task(__unused void *params) {
+    printf("    Hello from high priority thread.\n");
+    vTaskDelay(200);
+    while(1){
+        printf("    Trying to take semaphore in high priority thread.\n");
+        bool semaphore_pass = xSemaphoreTake(semaphore, portMAX_DELAY);
+        if(semaphore_pass){
+            printf("    We have the semaphore in high priority!\n");
+            for(int i = 0; i < 100000000; i++) {
+                if (i % 10000000 == 0);
+            }
+            xSemaphoreGive(semaphore);
+        }
+    }
+}
+
+void medium_priority_task(__unused void *params) {
+    printf("    Hello from medium priority thread.\n");
+    vTaskDelay(100);
+    while(1){
+        for(int i = 0; i < 100000000; i++) {
+            if (i % 10000000 == 0) {
+                printf("    Medium priority thread: counted to %d.\n", i);
+            }
+        }
+    }
+}
+
+void low_priority_task(__unused void *params) {
+    printf("    Hello from low priority thread.\n");
+    while(1){
+        printf("    Trying to take semaphore in low priority thread.\n");
+        bool semaphore_pass = xSemaphoreTake(semaphore, 500);
+        if(semaphore_pass){
+            printf("    We have the semaphore in low priority!\n");
+            for(int i = 0; i < 100000000; i++) {
+                if (i % 10000000 == 0);
+            }
+            xSemaphoreGive(semaphore);
+        }
+    }
+}
+
+void test_priority_inversion_binary(void)
+{
+    TaskHandle_t hp_task, mp_task, lp_task;
+    configRUN_TIME_COUNTER_TYPE low_run_time, medium_run_time, high_run_time;
+    semaphore = xSemaphoreCreateBinary();
+    xSemaphoreGive(semaphore);
+
+    xTaskCreate(low_priority_task, "Low Priority Thread",
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_LOW_PRIORITY, &lp_task);
+    xTaskCreate(medium_priority_task, "Medium Priority Thread",
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_MEDIUM_PRIORITY, &mp_task);
+    xTaskCreate(high_priority_task, "High Priority Thread",
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_HIGH_PRIORITY, &hp_task);
+    
+    vTaskDelay(5000);
+
+    low_run_time = ulTaskGetRunTimeCounter(lp_task);
+    medium_run_time = ulTaskGetRunTimeCounter(mp_task);
+    high_run_time = ulTaskGetRunTimeCounter(hp_task);
+
+    printf("  Low priority thread has run for %lld ticks.\n", low_run_time);
+    printf("  Medium priority thread has run for %lld ticks.\n", medium_run_time);
+    printf("  High priority thread has run for %lld ticks.\n", high_run_time);
+
+    TEST_ASSERT_MESSAGE(high_run_time < medium_run_time, "High-priority thread did not have its priority inverted.");
+
+    vTaskDelete(hp_task);
+    vTaskDelete(mp_task);
+    vTaskDelete(lp_task);
+}
+
+void test_priority_inversion_mutex(void)
+{
+    TaskHandle_t hp_task, mp_task, lp_task;
+    configRUN_TIME_COUNTER_TYPE low_run_time, medium_run_time, high_run_time;
+    // Using a mutex instead of a binary solves priority inversion 
+    //  because it implements priority inheritence.
+    semaphore = xSemaphoreCreateMutex();
+    xSemaphoreGive(semaphore);
+
+    xTaskCreate(low_priority_task, "Low Priority Thread",
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_LOW_PRIORITY, &lp_task);
+    xTaskCreate(medium_priority_task, "Medium Priority Thread",
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_MEDIUM_PRIORITY, &mp_task);
+    xTaskCreate(high_priority_task, "High Priority Thread",
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_HIGH_PRIORITY, &hp_task);
+    
+    vTaskDelay(5000);
+
+    low_run_time = ulTaskGetRunTimeCounter(lp_task);
+    medium_run_time = ulTaskGetRunTimeCounter(mp_task);
+    high_run_time = ulTaskGetRunTimeCounter(hp_task);
+
+    printf("  Low priority thread has run for %lld ticks.\n", low_run_time);
+    printf("  Medium priority thread has run for %lld ticks.\n", medium_run_time);
+    printf("  High priority thread has run for %lld ticks.\n", high_run_time);
+
+    TEST_ASSERT_MESSAGE(high_run_time > medium_run_time, "Priority was inverted; priority inheritance did not work.");
+
+    vTaskDelete(hp_task);
+    vTaskDelete(mp_task);
+    vTaskDelete(lp_task);
+}
 
 void test_same_priority_busy_busy(void)
 {
@@ -35,8 +144,11 @@ void test_same_priority_busy_busy(void)
     a_run_time = ulTaskGetRunTimeCounter(task_a);
     b_run_time = ulTaskGetRunTimeCounter(task_b);
 
-    printf("Thread thread A has run for %lld ticks.\n", a_run_time);
-    printf("Thread thread B has run for %lld ticks.\n", b_run_time);
+    printf("  Thread thread A has run for %lld ticks.\n", a_run_time);
+    printf("  Thread thread B has run for %lld ticks.\n", b_run_time);
+
+    TEST_ASSERT_MESSAGE(a_run_time > 2250000, "Thread A was starved.");
+    TEST_ASSERT_MESSAGE(b_run_time > 2250000, "Thread B was starved.");
 
     vTaskDelete(task_a);
     vTaskDelete(task_b);
@@ -57,8 +169,11 @@ void test_same_priority_yield_yield(void)
     a_run_time = ulTaskGetRunTimeCounter(task_a);
     b_run_time = ulTaskGetRunTimeCounter(task_b);
 
-    printf("Thread thread A has run for %lld ticks.\n", a_run_time);
-    printf("Thread thread B has run for %lld ticks.\n", b_run_time);
+    printf("  Thread thread A has run for %lld ticks.\n", a_run_time);
+    printf("  Thread thread B has run for %lld ticks.\n", b_run_time);
+
+    TEST_ASSERT_MESSAGE(a_run_time > 2250000, "Thread A was starved.");
+    TEST_ASSERT_MESSAGE(b_run_time > 2250000, "Thread B was starved.");
 
     vTaskDelete(task_a);
     vTaskDelete(task_b);
@@ -79,8 +194,12 @@ void test_same_priority_yield_busy(void)
     a_run_time = ulTaskGetRunTimeCounter(task_a);
     b_run_time = ulTaskGetRunTimeCounter(task_b);
 
-    printf("Thread thread A has run for %lld ticks.\n", a_run_time);
-    printf("Thread thread B has run for %lld ticks.\n", b_run_time);
+    printf("  Thread thread A has run for %lld ticks.\n", a_run_time);
+    printf("  Thread thread B has run for %lld ticks.\n", b_run_time);
+
+    
+    TEST_ASSERT_MESSAGE(b_run_time < a_run_time, "Yield thread did not yield.");
+    TEST_ASSERT_MESSAGE(a_run_time > 4500000, "Busy thread did not run enough.");
 
     vTaskDelete(task_a);
     vTaskDelete(task_b);
@@ -92,9 +211,9 @@ void test_diff_priority_busy_busy_high_first(void)
     configRUN_TIME_COUNTER_TYPE a_run_time, b_run_time;
 
     xTaskCreate(busy_busy, "Thread A",
-                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_MEDIUM_PRIORITY, &task_a);
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_HIGH_PRIORITY, &task_a);
 
-    vTaskDelay(200);
+    vTaskDelay(10);
 
     xTaskCreate(busy_busy, "Thread B",
                 TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_LOW_PRIORITY, &task_b);
@@ -104,8 +223,11 @@ void test_diff_priority_busy_busy_high_first(void)
     a_run_time = ulTaskGetRunTimeCounter(task_a);
     b_run_time = ulTaskGetRunTimeCounter(task_b);
 
-    printf("Thread thread A has run for %lld ticks.\n", a_run_time);
-    printf("Thread thread B has run for %lld ticks.\n", b_run_time);
+    printf("  Thread thread A has run for %lld ticks.\n", a_run_time);
+    printf("  Thread thread B has run for %lld ticks.\n", b_run_time);
+
+    TEST_ASSERT_MESSAGE(a_run_time > b_run_time, "High priority thread did not run enough.");
+    TEST_ASSERT_MESSAGE(b_run_time < 50, "Low priority thread ran when it should not have been preempted.");
 
     vTaskDelete(task_a);
     vTaskDelete(task_b);
@@ -119,18 +241,21 @@ void test_diff_priority_busy_busy_low_first(void)
     xTaskCreate(busy_busy, "Thread A",
                 TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_LOW_PRIORITY, &task_a);
 
-    vTaskDelay(200);
+    vTaskDelay(10);
 
     xTaskCreate(busy_busy, "Thread B",
-                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_MEDIUM_PRIORITY, &task_b);
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_HIGH_PRIORITY, &task_b);
 
     vTaskDelay(5000);
 
     a_run_time = ulTaskGetRunTimeCounter(task_a);
     b_run_time = ulTaskGetRunTimeCounter(task_b);
 
-    printf("Thread thread A has run for %lld ticks.\n", a_run_time);
-    printf("Thread thread B has run for %lld ticks.\n", b_run_time);
+    printf("  Thread thread A has run for %lld ticks.\n", a_run_time);
+    printf("  Thread thread B has run for %lld ticks.\n", b_run_time);
+
+    TEST_ASSERT_MESSAGE(b_run_time > a_run_time, "High priority thread did not run enough.");
+    TEST_ASSERT_MESSAGE(a_run_time < 20000, "Low priority thread ran for too long.");
 
     vTaskDelete(task_a);
     vTaskDelete(task_b);
@@ -142,7 +267,7 @@ void test_diff_priority_yield_yield(void)
     configRUN_TIME_COUNTER_TYPE a_run_time, b_run_time;
 
     xTaskCreate(busy_yield, "Thread A",
-                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_MEDIUM_PRIORITY, &task_a);
+                TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_HIGH_PRIORITY, &task_a);
     xTaskCreate(busy_yield, "Thread B",
                 TEST_THREAD_STACK_SIZE, NULL, TEST_THREAD_LOW_PRIORITY, &task_b);
 
@@ -151,8 +276,11 @@ void test_diff_priority_yield_yield(void)
     a_run_time = ulTaskGetRunTimeCounter(task_a);
     b_run_time = ulTaskGetRunTimeCounter(task_b);
 
-    printf("Thread thread A has run for %lld ticks.\n", a_run_time);
-    printf("Thread thread B has run for %lld ticks.\n", b_run_time);
+    printf("  Thread thread A has run for %lld ticks.\n", a_run_time);
+    printf("  Thread thread B has run for %lld ticks.\n", b_run_time);
+
+    TEST_ASSERT_MESSAGE(a_run_time > b_run_time, "High priority thread did not run enough.");
+    TEST_ASSERT_MESSAGE(b_run_time < 50, "Low priority thread ran when it should not have been preempted.");
 
     vTaskDelete(task_a);
     vTaskDelete(task_b);
@@ -163,6 +291,8 @@ void test_run_thread(void *args)
     while(1) {
         printf("Start tests\n");
         UNITY_BEGIN();
+        RUN_TEST(test_priority_inversion_binary);
+        RUN_TEST(test_priority_inversion_mutex);
         RUN_TEST(test_same_priority_busy_busy);
         RUN_TEST(test_same_priority_yield_yield);
         RUN_TEST(test_same_priority_yield_busy);
@@ -177,6 +307,7 @@ void test_run_thread(void *args)
 int main (void)
 {
     stdio_init_all();
+    hard_assert(cyw43_arch_init() == PICO_OK);
     sleep_ms(5000); // Give time for TTY to attach.
     xTaskCreate(test_run_thread, "RunTests",
         configMINIMAL_STACK_SIZE, NULL, TEST_RUNNER_PRIORITY, NULL);
